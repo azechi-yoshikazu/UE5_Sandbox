b9adcdfd90029b064418694c430e26443e70d4cc
YoshikazuAzechi <walkingcat.cute@gmail.com>
2022-04-15 01:43:33 +0900

    Added widget for debugger tab to show toggle fps button.

--- Plugins/MyToolBox/Source/MyToolBox/Private/SMyToolBoxTab.cpp
+++ Plugins/MyToolBox/Source/MyToolBox/Private/SMyToolBoxTab.cpp
@@ -2,12 +2,57 @@
 
 #include "SMyToolBoxTab.h"
 
+// Engine
+#include "Engine/World.h"
+#include "Widgets/SCompoundWidget.h"
+
 #define LOCTEXT_NAMESPACE "MyToolBox"
 
 namespace MyToolBox
 {
     namespace DebuggerTab
     {
+        class SDebuggerWidget : public SCompoundWidget
+        {
+        public:
+            SLATE_BEGIN_ARGS(SDebuggerWidget) {}
+            SLATE_END_ARGS()
+
+            SDebuggerWidget() : SCompoundWidget() {}
+            virtual ~SDebuggerWidget() {}
+        public:
+            void Construct(const FArguments& InArgs)
+            {
+                TArray<IConsoleCommandExecutor*> CommandExecutors = IModularFeatures::Get().GetModularFeatureImplementations<IConsoleCommandExecutor>(IConsoleCommandExecutor::ModularFeatureName());
+
+                CommandExecutor = CommandExecutors.Num() > 0 ? CommandExecutors[0] : nullptr;
+
+                ChildSlot
+                    [
+                        SNew(SButton)
+                        .Text(LOCTEXT("MyToolBox.Debug.ShowFPS", "Toggle FPS"))
+                        .OnClicked_Raw<SDebuggerWidget, TCHAR*>(this, &SDebuggerWidget::ExecuteCommand, TEXT("stat fps"))
+                    ];
+            }
+
+        protected:
+            FReply ExecuteCommand(TCHAR* Command)
+            {
+                if (CommandExecutor != nullptr)
+                {
+                    if (CommandExecutor->Exec(Command))
+                    {
+                        return FReply::Handled();
+                    }
+                }
+
+                return FReply::Unhandled();
+            }
+
+        protected:
+            IConsoleCommandExecutor* CommandExecutor = nullptr;
+        };
+
         void RegisterTabSpawner(const TSharedPtr<FTabManager>& TabManager)
         {
             TabManager->RegisterTabSpawner(
@@ -19,13 +64,7 @@ namespace MyToolBox
                         .TabRole(ETabRole::PanelTab)
                         .Label(LOCTEXT("MyToolBox.DebuggerTab.Title", "Debugger"))
                         [
-                            SNew(SBox)
-                            .VAlign(VAlign_Center)
-                            .HAlign(HAlign_Center)
-                            [
-                                SNew(STextBlock)
-                                .Text(LOCTEXT("MyToolBox.DebuggerTab.Content", "This is Debugger"))
-                            ]
+                            SNew(SDebuggerWidget)
                         ];
                 })
             )
